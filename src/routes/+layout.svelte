783a829967b5ee616ef32e0e919d6f9ae53a8ada
Chi-Feng Tsai <ktpss95112@gmail.com>
2025-11-14 11:16:00 +0800

    update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import '../app.css';
-    import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/favicon.png';
     import { page } from '$app/state';
     import { resolve, asset } from '$app/paths';
 
